13089ae48a4f15169635fce9d1fea15b3ad3f43b
mat-lo <write@matlo.me>
2025-11-19 15:38:18 +0100

    yoo

---
+++ src/lib/components/Pill.svelte
@@ -0,0 +1,9 @@
+<script>
+	let { text, isBlue } = $props();
+</script>
+
+<div class="rounded-full {isBlue ? 'bg-ulb' : 'bg-ye'} px-3 py-0">
+	<p class="text-center font-comic text-lg font-bold {isBlue ? 'text-white' : 'text-ulb'}">
+		{text}
+	</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import bgImage from '$lib/assets/bg.webp';
+	import title from '$lib/assets/title.webp';
+	import ballImg from '$lib/assets/ball.webp';
+	import Drop from '$lib/components/Drop.svelte';
+</script>
+
+<div class="relative min-h-screen w-full">
+	<img
+		src={bgImage}
+		alt="Background"
+		class="absolute top-0 left-0 z-0 h-full w-full object-cover"
+	/>
+
+	<div
+		class="relative z-10 mx-auto flex h-screen w-2xl flex-col items-center justify-start gap-6 pt-18"
+	>
+		<div class="relative flex w-md items-center justify-center">
+			<img
+				src={title}
+				alt="Title"
+				class="absolute top-1/2 left-1/2 z-10 w-full -translate-x-1/2 -translate-y-1/2"
+			/>
+			<img src={ballImg} alt="Ball" class="relative z-0 w-2/3" />
+		</div>
+		<p class="w-2/3 text-center font-comic text-white">
+			wef aegrg raeg r wef aegrg raeg r wef aegrg raeg r wef aegrg raeg r wef aegrg raeg r wef aegrg
+			raeg r wef aegrg raeg r wef aegrg raeg r wef aegrg raeg r wef aegrg raeg r
+		</p>
+
+		<Drop />
+	</div>
+</div>
